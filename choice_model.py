610a835ad3ff3a6676898eff13ad95e497502de2
kekehurry <arhukai1111@gmail.com>
2024-07-21 23:01:56 -0400

    init

---
+++ choice_model.py
@@ -0,0 +1,153 @@
+import faiss
+import networkx as nx
+import pickle
+import numpy as np
+import pandas as pd
+from langchain_community.embeddings import OllamaEmbeddings
+import uuid
+
+class ChoiceModel:
+    def __init__(self,
+        graph_path="community_graph_embeded.pkl",
+        index_path="index.pkl",
+        embed_model="nomic-embed-text",
+        chat_model="phi3-instruct",
+        embedding_dimension=768
+        ):
+        self.embed_model = OllamaEmbeddings(model=embed_model)
+        self.embedding_dimension = embedding_dimension
+        self._graph = self._load_graph(graph_path)
+        self.set_period()
+        
+    def _load_graph(self, graph_path):
+        with open(graph_path, "rb") as f:
+            G = pickle.load(f)
+        return G
+    
+    def _load_index(self, index_path):
+        with open("index.pkl", "rb") as f:
+            index = pickle.load(f)
+        return index
+    
+    def _cal_degree_centralities(self):
+        degree_centralities = nx.degree_centrality(self.graph)
+        return degree_centralities
+
+    def set_period(self, period=None):
+        if period:
+            related_links = [link for link in self._graph.edges() if self._graph.edges[link]["period"] == period]
+            self.graph = self._graph.edge_subgraph(related_links).copy()
+        else:
+            self.graph = self._graph.copy()
+        node_embeddings = np.array([node['embedding'] for idx,node in self.graph.nodes(data=True)])
+        index = faiss.IndexFlatL2(self.embedding_dimension)
+        index.add(node_embeddings)
+        ids = np.array([node for node in self.graph.nodes()])
+        self.index = {"index":index, "ids":ids}
+        self.degree_centralities = self._cal_degree_centralities()
+        self.period = period
+
+    
+    def similarity_search(self,query, k1=5, k2=2,node_type='Actors'):
+
+        # step 1: find nodes with similar profile
+        def semantic_similarity_search(query, k1):
+            query = str(query)
+            query_embed = self.embed_model.embed_query(query)
+            query_embed = np.array(query_embed).reshape(1,-1)
+            d, i = self.index["index"].search(query_embed, k1)
+            similar_nodes = self.index["ids"][i][0]
+            simiilar_scores = d[0]
+            return similar_nodes,simiilar_scores
+
+        # step 2: find nodes with similar degree_centrality in the community network
+        def social_similarity_search(node_id,k2,node_type='Actors'):
+            # filter all nodes with same type
+            same_type_nodes = [node for node in self.graph.nodes() if self.graph.nodes[node]["type"] == node_type]
+            node_degree_centrality = self.degree_centralities[node_id]
+            node_degree_centralities = {k:abs(v-node_degree_centrality) for k, v in self.degree_centralities.items() if k in same_type_nodes}
+            sorted_nodes = sorted(node_degree_centralities.items(), key=lambda x: x[1])
+            sorted_similar_nodes = sorted_nodes[:k2]
+            similar_nodes = [node for node, _ in sorted_similar_nodes]
+            similar_scores = [score for _, score in sorted_similar_nodes]
+            return similar_nodes,similar_scores
+
+        # combine the two similarity search results
+        nodes = []
+        scores = []
+        senmantic_similar_nodes,senmantic_similar_scores = semantic_similarity_search(query, k1)
+        for node1,score1 in zip(senmantic_similar_nodes,senmantic_similar_scores):
+            social_similar_nodes,social_similar_scores = social_similarity_search(node1,k2,node_type)
+            filtered_nodes = [n for n in social_similar_nodes if n not in nodes]
+            filtered_scores = [score1+score2 for score2,n in zip(social_similar_scores,social_similar_nodes) if n not in nodes]
+            nodes.extend(filtered_nodes)
+            scores.extend(filtered_scores)
+        return nodes,scores
+
+    def link_prediction(self, profile, k1, k2, node_type='Actors', choice_type='Actors'):
+
+        def softmax(x):
+            e_x = np.exp(x - np.max(x))
+            return e_x / e_x.sum()
+
+        def cal_weighted_jaccard_coeff(G,agent,choice):
+            G_ = G.to_undirected()
+            common_neighbors = list(nx.common_neighbors(G_,agent,choice))
+            total_neighbors = len(list(nx.neighbors(G_,agent)))
+            weight_sum = 0
+            for n in common_neighbors:
+                edge_ap = G_.edges[agent,n]['weight']
+                edge_pc = G_.edges[n,choice]['weight']
+                weight_sum += (edge_ap+edge_pc)/2
+            return weight_sum/total_neighbors
+
+        def get_recommendation(G,choice_type):
+            agents = [n for n,d in G.nodes(data=True) if d['type'] == 'Agent']
+            choices = [n for n,d in G.nodes(data=True) if d['type'] == choice_type]
+            recommendation = {}
+            for choice in choices:
+                recommendation[choice] = cal_weighted_jaccard_coeff(G,agents[0],choice)
+            recommendation = {k: v for k, v in sorted(recommendation.items(), key=lambda item: item[1],reverse=True)}
+            choices = list(recommendation.keys())
+            choices = [G.nodes[n] for n in choices]
+            probabilities = softmax(list(recommendation.values())).tolist()
+            return choices,probabilities
+
+        # step 1: find nodes with similar profile
+        similar_nodes,similar_scores = self.similarity_search(profile, k1, k2, node_type)
+
+        # step 2: find neighbors of similar nodes
+        neighbors = []
+        for n in similar_nodes:
+            neighbors.extend(list(self.graph.neighbors(n)))
+            neighbors.append(n) 
+
+        # step 3: create compute graph
+        compute_graph = nx.subgraph_view(self.graph, filter_node=lambda n: n in neighbors).copy()
+        # normalize the weights of links
+        weights = nx.get_edge_attributes(compute_graph, 'weight')
+        max_weight = max(weights.values())
+        for e in compute_graph.edges():
+            compute_graph[e[0]][e[1]]['weight'] = compute_graph[e[0]][e[1]]['weight'] / max_weight
+        # normalize the weights of similarity scores
+        max_score = max(similar_scores)
+        similar_scores = [s / max_score for s in similar_scores]
+
+        #step 4: add agent node
+        agent_id = uuid.uuid4().hex
+        compute_graph.add_node(
+            agent_id, 
+            type='Agent', 
+            period = self.period, 
+            properties=profile,
+            label='Agent'
+        )
+        for node,scocre in zip(similar_nodes,similar_scores):
+            compute_graph.add_edge(agent_id, node, weight=scocre, type='similar_to',label='similar_to') 
+        
+        # step 5: get recommendation
+        choices,probabilities = get_recommendation(compute_graph,choice_type)
+
+        return choices,probabilities
+
+    
